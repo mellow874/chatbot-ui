8e6d7eeab6197cc204c12eb28327635994d01962
mellow874 <felicitymncube49@gmail.com>
2026-05-13 22:13:07 +0200

    removed dead code

--- types/chat-message.ts
+++
@@ -1,6 +0,0 @@
-import { Tables } from "@/supabase/types"
-
-export interface ChatMessage {
-  message: Tables<"messages"> & { sources?: { title: string; url?: string; source_type?: string }[] }
-  fileItems: string[]
-}
